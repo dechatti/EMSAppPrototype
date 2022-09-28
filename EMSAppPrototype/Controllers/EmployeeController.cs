48ea8146b08c838024778ec49e6f261c864d7af5
dechatti <105226010+dechatti@users.noreply.github.com>
2022-09-28 16:56:39 +0100

    Add project files.

---
+++ EMSAppPrototype/Controllers/EmployeeController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EMSAppPrototype.EntityModels;
+using EMSAppPrototype.Models;
+
+namespace EMSAppPrototype.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EMSDbPtContext db = new EMSDbPtContext();
+            List<Employee> employees = db.Employees.ToList();
+            return View(employees);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            EmployeeViewModel employee = new EmployeeViewModel();
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmployeeViewModel model)
+        {
+            try
+            {
+                EMSDbPtContext db = new EMSDbPtContext();
+                Employee emp = new Employee
+                {
+                    Address = model.Address,
+                    DateCreated = DateTime.Now,
+                    Email = model.Email,
+                    Phone = model.Phone,
+                    Salary = model.Salary,
+                    IsActive = true
+                };
+                db.Employees.Add(emp);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Error = "An Error Occured while trying to create your account. Please try again later";
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return View("Error");
+            int ID = Helper.Decrypt(id);
+            EMSDbPtContext db = new EMSDbPtContext();
+            Employee employee = db.Employees.FirstOrDefault(x => x.Id == ID);
+            return View(employee);
+        }
+        [HttpPost]
+        public ActionResult Edit(EmployeeViewModel model)
+        {
+            try
+            {
+                EMSDbPtContext db = new EMSDbPtContext();
+                var originalEmployee = db.Employees.FirstOrDefault(x => x.Id == model.Id);
+                originalEmployee.Email = model.Email;
+                originalEmployee.Address = model.Address;
+                originalEmployee.Phone = model.Phone;
+                originalEmployee.Salary = model.Salary;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error("An error occured while trying to update your account details. Please try again later.");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Delete(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return View("Error");
+            int ID = Helper.Decrypt(id);
+            EMSDbPtContext db = new EMSDbPtContext();
+            Employee employee = db.Employees.FirstOrDefault(x => x.Id == ID);
+            return View(employee);
+        }
+        [HttpPost]
+        public ActionResult Delete(Employee employee)
+        {
+            try
+            {
+                EMSDbPtContext db = new EMSDbPtContext();
+
+                Employee emp = db.Employees.FirstOrDefault(x => x.Id == employee.Id);
+                db.Employees.Remove(emp);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "An error occured while trying to delete employee record.";
+            }
+            return View(employee);
+        }
+    }
+}
---
+++ EMSAppPrototype/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EMSAppPrototype")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EMSAppPrototype")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6650b84a-c0e0-483f-8bc2-1e8085525c4a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
